a6bad8993b5208d5f11afd597d3598a499be53dd
Allison Harden <harden.allison@gmail.com>
2021-08-20 20:19:19 -0600

    Working routes, get, post, delete. No update.

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,15 +1,35 @@
 const router = require('express').Router();
-//const { createNewAnimal, validateAnimal } = require('../../lib/animals');
-const { notes } = require('../../data/notes');
+const notes = require('../../data/notes');
+const {createNewNote, findById, deleteNote} = require('../../lib/notes');
 
-router.get('api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
+  //console.log(notes);
   res.json(notes);
 });
 
-router.post('api/notes', (req, res) => {
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const result = deleteNote(req.params.id, notes);;
+  console.log(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404)
+  }
+});
+
+router.post('/notes', (req, res) => {
   req.body.id = notes.length.toString();
-    const note = req.body;
-    res.json(note);
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 });
 
 module.exports = router;
